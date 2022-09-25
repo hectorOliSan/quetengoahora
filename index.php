04c0fd54edf7cf8e9f77ce011c520f0da50af37c
Héctor Olivares Sánchez <hectorolivares35@gmail.com>
2022-09-25 17:32:59 +0100

    Versión 1.2.4

--- index.php
+++ index.php
@@ -4,9 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <title>Horario DAW</title>
-
     <style type="text/css">
       * {
         font-family: consolas;
@@ -41,7 +39,6 @@
         background: LightSlateGray;
       }
     </style>
-
     <script language="JavaScript">
       function updateReloj() {
         hora =
